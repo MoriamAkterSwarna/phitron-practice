523757dea384ba3402a8e9de0c8136e59645ad8c
MoriamAkterSwarna <moriam15-12880@diu.edu.bd>
2024-06-12 21:27:28 +0600

    module 3 ds prefix sum added

---
+++ Data Structure/Week-01/Module-03/Y_Range_sum_query.cpp
@@ -0,0 +1,43 @@
+// with prefix sum 
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+   long long  int n,q; 
+    cin>>n>>q;                                                                        
+
+    long long int a[n]; 
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    } 
+
+    int pre[n]; 
+    pre[0] = a[0]; 
+
+    for(int i=1;i<n;i++){
+        pre[i] = pre[i-1] + a[i];
+    }
+    while(q--){
+        long long int l,r;
+        cin>>l>>r;
+        l--; r--; // 0 based indexing
+
+        long long int sum = 0;
+        
+        if(l==0)
+            sum = pre[r];
+        else
+            sum = pre[r] - pre[l-1]; // prefix sum of r - prefix sum of l-1 (l to r)
+
+
+        cout<<sum<<endl;
+    }
+
+
+
+
+    return 0;
+}
---
+++ Data Structure/Week-01/Module-03/prefix_sum.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int n,q; 
+    cin>>n>>q;                                                                        
+
+    int a[n]; 
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    } 
+
+    while(q--){
+        int l,r;
+        cin>>l>>r;
+        l--; r--; // 0 based indexing
+
+        int sum = 0;
+        for(int i=l;i<=r;i++){
+            sum += a[i];
+        }
+
+        cout<<sum<<endl;
+    }
+
+
+
+
+    return 0;
+}
