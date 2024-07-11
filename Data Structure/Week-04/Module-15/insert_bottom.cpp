6e3c63e8af9bcd074040b69f35dc74f70049bfae
MoriamAkterSwarna <moriam15-12880@diu.edu.bd>
2024-07-12 00:02:04 +0600

    some problems solving added from leetcode & condingninja

---
+++ Data Structure/Week-04/Module-15/insert_bottom.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> 
+stack<int> pushAtBottom( stack<int>& myStack , int x )
+    {
+    // Write your code here.
+    stack<int> newSt;
+    while ( !myStack.empty( ) ) {
+        newSt.push( myStack.top( ) );
+        myStack.pop( );
+        }
+    newSt.push( x );
+    while ( !newSt.empty( ) ) {
+        myStack.push( newSt.top( ) );
+        newSt.pop( );
+        }
+    return myStack;
+    }
