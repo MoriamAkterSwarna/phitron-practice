319f763c01e0b1c99724d29a3e93157478afdefc
MoriamAkterSwarna <moriam15-12880@diu.edu.bd>
2024-10-06 23:08:10 +0600

    0-1 knapsack added

---
+++ Algorithm/Week-5/Module-17/0-1_knapsack.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int knapsack(int n, int weight[], int value[], int w){
+
+    if(n <= 0 || w == 0){
+        return 0;
+    }
+
+    if(weight[n] <= w){
+
+        // 2 ta option 
+        //  niye dekhbo, na niye dekhbo 
+
+        int op1 = knapsack(n-1, weight, value, w-weight[n] ) + value[n];
+
+        int op2 = knapsack(n-1, weight, value, w);
+
+        return max(op1 , op2);
+        
+    } 
+    else{
+
+        // ekta option 
+        // na niye dekhbo 
+
+       int op2 = knapsack(n-1, weight, value, w);
+
+         return op2;
+    }
+
+    
+
+     
+}
+
+int main(){
+
+    int n; 
+    cin >> n; 
+    int weight[n], value[n]; 
+    for(int i = 0; i < n; i++){
+        cin >> weight[i];
+    } 
+
+    for(int i = 0; i < n; i++){
+        cin >> value[i];
+    } 
+
+    int w; 
+    cin >> w; 
+
+    cout<< knapsack(n-1, weight, value, w) << endl; 
+
+    return 0;
+}
